1ff1c94560c319b325c51a68a205f52e1331ee71
sanjayramabuddi <sanjayramabuddi@gmail.com>
2025-08-01 00:16:34 +0530

    react preparation started

---
+++ apps/react-app/src/components/TodoList.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+function TodoList() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function addTodoItem() {
+    if (!input.trim()) return;
+
+    const item = {
+      id: Date.now(),
+      text: input,
+      completed: false
+    }
+    setTodos(prev => [...prev, item]);
+    setInput('');
+  }
+
+  function toggleCompleted(id) {
+    setTodos(prev =>
+      prev.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    )
+  }
+
+  function removeTodo(id) {
+    setTodos(prev => prev.filter(prev => prev.id !== id))
+  }
+
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <input
+        type="text"
+        placeholder="Enter todo"
+        onChange={(e) => setInput(e.target.value)}
+        value={input} />
+      <button
+        onClick={addTodoItem}>
+        Add
+      </button>
+      <ul>
+        {
+          todos.map((todo) =>
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={() => toggleCompleted(todo.id)} />
+              <span
+                className={todo.completed ? "striked" : ""}>
+                {todo.text}
+              </span>
+              <button
+                onClick={() => removeTodo(todo.id)}>
+                Delete
+              </button>
+            </li>
+          )
+        }
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
--- apps/web/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { add } from "@repo/math/add";
-import { Admin } from "@repo/ui/admin";
-
-export default function Home() {
-  return <div>{add(1, 2)}
-  <Admin />
-  </div>;
-}
