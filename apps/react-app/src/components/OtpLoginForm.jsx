28c75ebd6ca4ac737bc069a084719a20820c9f64
sanjayramabuddi <sanjayramabuddi@gmail.com>
2025-08-03 23:57:32 +0530

    added otp input comp

--- apps/react-app/src/components/OtpLoginForm.jsx
+++ apps/react-app/src/components/OtpLoginForm.jsx
@@ -1,10 +1,44 @@
-import Otp from "./Otp";
+import { useState } from "react";
+import OtpInput from "./OtpInput";
+import "./component.css";
 
 export default function OtpLoginForm() {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [showOtpInput, setShowOtpInput] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const regex = /[^0-9]/g;
+    if (phoneNumber.length < 10 || regex.test(phoneNumber)) {
+      alert("Invalid phone number");
+      return;
+    }
+
+    setShowOtpInput(true);
+  }
+
+  function onOtpSubmit() {}
+
   return (
-    <div>
-      <h1>Login Form</h1>
-      <Otp />
+    <div className="app">
+      {!showOtpInput ? (
+        <form onSubmit={handleSubmit}>
+          <h1>Login Form</h1>
+          <input
+            type="text"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            value={phoneNumber}
+            placeholder="Enter phone number"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div>
+          <p>Enter OTP sent to {phoneNumber}</p>
+          <OtpInput length={4} onOtpSubmit={onOtpSubmit} />
+        </div>
+      )}
     </div>
   );
 }
